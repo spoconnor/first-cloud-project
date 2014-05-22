aed9f68394e33f3b3322edbef532d8570dc8a336
Sean <onewheel@gmail.com>
2014-05-22 19:20:54 +1200

    Test Rest project added

---
+++ TestWorldRest/Main.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ServiceModel;
+
+namespace TestWorldRest
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+            Uri baseAddress = new Uri("http://localhost:8888/GettingStarted/");
+            ServiceHost selfHost = new ServiceHost(typeof(voxelworld.WorldServiceImpl), baseAddress);
+            
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IWorldService), new WebHttpBinding(), "WorldService");
+                
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Abort();
+            }
+            */
+        }
+    }
+}
