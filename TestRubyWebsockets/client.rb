4b4065fb20d059cce4b7b10f87657ccba8101ed7
Sean <onewheel@gmail.com>
2014-10-19 19:55:48 +1300

    ruby websocket client

---
+++ TestRubyWebsockets/client.rb
@@ -0,0 +1,32 @@
+# Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
+# Lincense: New BSD Lincense
+
+$LOAD_PATH << File.dirname(__FILE__) + "/lib"
+require "web_socket"
+
+#if ARGV.size != 1
+#  $stderr.puts("Usage: ruby samples/stdio_client.rb ws://HOST:PORT/")
+#  exit(1)
+#end
+
+#begin
+
+#client = WebSocket.new(ARGV[0])
+client = WebSocket.new("ws://10.1.1.4:8081")
+puts("Connected")
+Thread.new() do
+  while data = client.receive()
+    printf("Received: %p\n", data)
+  end
+  exit()
+end
+$stdin.each_line() do |line|
+  data = line.chomp()
+  client.send(data)
+  printf("Sent: %p\n", data)
+end
+client.close()
+
+#rescue Exception => e
+# puts "Exception #{e}"
+#end
