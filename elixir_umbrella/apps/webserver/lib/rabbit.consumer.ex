4a729d041350385d7c9727025cb03bcf81d8b523
Sean <onewheel@gmail.com>
2014-10-28 22:15:56 +1300

    rabbit

--- elixir_umbrella/apps/webserver/lib/rabbit.consumer.ex
+++ elixir_umbrella/apps/webserver/lib/rabbit.consumer.ex
@@ -1,86 +1,109 @@
 defmodule Rabbit.Consumer do
-@behaviour :gen_bunny
+ use Exrabbit.Subscriber
+
+  def start_link() do
+    :gen_server.start_link(Rabbit.Consumer,
+                      [ queue: "testQ"],
+                      #[host: "localhost",
+                      # port: 5672,
+                      # username: "guest",
+                      # password: "guest",
+                      # queue: "testQ"],
+                      [])
+  end
+
+  def handle_message(msg, state) do
+    case parse_message(msg) do
+      nil -> IO.puts "Got nil message"
+      {tag, payload} ->
+        IO.puts "Got message with tag #{tag} and payload #{payload}"
+        ack state[:channel], tag
+    end
+  end
+
+
+#@behaviour :gen_bunny
 
 #include_lib("eunit/include/eunit.hrl").
 #export([ack_stuff/2, get_messages/1]).
 #include("deps/gen_bunny/include/gen_bunny.hrl").
 
-defmodule State do
- defstruct(
-   messages: [], 
-   calls: [],
-   infos: [],
-   casts: [],
-   wspid: []
- )
-end
+#defmodule State do
+# defstruct(
+#   messages: [], 
+#   calls: [],
+#   infos: [],
+#   casts: [],
+#   wspid: []
+# )
+#end
 
-def start_link(webSocketPid) do
-  [a,b] = tl(:string.tokens(:erlang.pid_to_list(self()),"<>."))
-  nameAsList = "consumer-" ++ a ++ "-" ++ b
-  nameAsAtom = :erlang.list_to_atom(nameAsList)
-  nameAsBin = :erlang.list_to_binary(nameAsList)
-  exchange =  :exchange.declare(exchange: <<"fanout">>, type: <<"fanout">>, durable: :true)
-  que = :queue.declare(queue: nameAsBin)
-  routingKey = <<"">>
-  connectInfo = {:network, "localhost", 5672, {<<"guest">>, <<"guest">>}, <<"/">>}
-  declareInfo = {exchange, que, routingKey}
-  :gen_bunny.start_link({:local, nameAsAtom}, __MODULE__, connectInfo, declareInfo, [webSocketPid])
-end
+#def start_link(webSocketPid) do
+#  [a,b] = tl(:string.tokens(:erlang.pid_to_list(self()),"<>."))
+#  nameAsList = "consumer-" ++ a ++ "-" ++ b
+#  nameAsAtom = :erlang.list_to_atom(nameAsList)
+#  nameAsBin = :erlang.list_to_binary(nameAsList)
+#  exchange =  :exchange.declare(exchange: <<"fanout">>, type: <<"fanout">>, durable: :true)
+#  que = :queue.declare(queue: nameAsBin)
+#  routingKey = <<"">>
+#  connectInfo = {:network, "localhost", 5672, {<<"guest">>, <<"guest">>}, <<"/">>}
+#  declareInfo = {exchange, que, routingKey}
+#  :gen_bunny.start_link({:local, nameAsAtom}, __MODULE__, connectInfo, declareInfo, [webSocketPid])
+#end
 
-def init([args]) do
-  IO.format("~p: init", [__MODULE__])
-  IO.format("Args: ~p~n", [args])
-  {:ok, %State{wspid: args}}
-end
+#def init([args]) do
+#  IO.format("~p: init", [__MODULE__])
+#  IO.format("Args: ~p~n", [args])
+#  {:ok, %State{wspid: args}}
+#end
 
-def init(_one,_two) do
-  IO.format("INIT TWO~n")
-  {:ok, %State{}}
-end
+#def init(_one,_two) do
+#  IO.format("INIT TWO~n")
+#  {:ok, %State{}}
+#end
 
-def ack_stuff(pid, tag) do
-  :gen_bunny.cast(pid, {:ack_stuff, tag})
-end
+#def ack_stuff(pid, tag) do
+#  :gen_bunny.cast(pid, {:ack_stuff, tag})
+#end
 
-def get_messages(pid) do
-  :gen_bunny.call(pid, :get_messages)
-end
+#def get_messages(pid) do
+#  :gen_bunny.call(pid, :get_messages)
+#end
   
-def stop(pid) do
-  :gen_bunny.call(pid, :stop)
-end
+#def stop(pid) do
+#  :gen_bunny.call(pid, :stop)
+#end
 
-def handle_message(message, %State{messages: messages, wspid: wsPid}) do # TODO # when is_message?(message) or is_tagged_message?(message) do
-  {:amqp_msg, _rest , payload} = message
-  IO.format("| ~p going to send payload: ~p to ~p~n", [__MODULE__, payload, wsPid])
-  send wsPid,  {:rabbit,payload}
-  newMessages = [message|messages]
-  {:noreply, %State{messages: newMessages}}
-end
+#def handle_message(message, %State{messages: messages, wspid: wsPid}) do # TODO # when is_message?(message) or is_tagged_message?(message) do
+#  {:amqp_msg, _rest , payload} = message
+#  IO.format("| ~p going to send payload: ~p to ~p~n", [__MODULE__, payload, wsPid])
+#  send wsPid,  {:rabbit,payload}
+#  newMessages = [message|messages]
+#  {:noreply, %State{messages: newMessages}}
+#end
 
-def handle_message(message, state) do
-  IO.format("Got rubbish: ~p~n", [message])
-  {:norely, state}
-end
+#def handle_message(message, state) do
+#  IO.format("Got rubbish: ~p~n", [message])
+#  {:norely, state}
+#end
 
-def handle_call(:get_messages, _from, _state=%State{messages: messages}) do
-  IO.format("Getting messages~n", [])
-  {:reply, messages, %State{messages: []}}
-end
+#def handle_call(:get_messages, _from, _state=%State{messages: messages}) do
+#  IO.format("Getting messages~n", [])
+#  {:reply, messages, %State{messages: []}}
+#end
 
-def handle_cast(msg, %State{casts: casts}) do
-  {:noreply, %State{casts: [msg|casts]}}
-end
+#def handle_cast(msg, %State{casts: casts}) do
+#  {:noreply, %State{casts: [msg|casts]}}
+#end
 
-def handle_info(info, %State{infos: infos}) do
-  IO.format("INFO~n")
-  {:noreply, %State{infos: [info|infos]}}
-end
+#def handle_info(info, %State{infos: infos}) do
+#  IO.format("INFO~n")
+#  {:noreply, %State{infos: [info|infos]}}
+#end
 
-def terminate(reason, _state) do
-  IO.format("~p terminating with reason ~p~n", [__MODULE__, reason])
-  :ok
-end
+#def terminate(reason, _state) do
+#  IO.format("~p terminating with reason ~p~n", [__MODULE__, reason])
+#  :ok
+#end
 
 end
--- elixir_umbrella/apps/webserver/lib/rabbit.producer.ex
+++ elixir_umbrella/apps/webserver/lib/rabbit.producer.ex
@@ -1,76 +1,119 @@
 defmodule Rabbit.Producer do
+  #use Behaviour
+  use GenServer
+
+  defmodule RabbitProducerState do
+    defstruct( amqp: nil, channel: nil, seq: 0 )
+  end
+
+  def start_link() do
+    #:gen_server.start_link({ :local, :Rabbit.Producer }, __MODULE__, [])
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def init(:ok) do
+    amqp = Exrabbit.Utils.connect()
+      #host: "localhost",
+      #port: 5672,
+      #username: "guest",
+      #password: "guest")
+
+    channel = Exrabbit.Utils.channel amqp
+    queue = Exrabbit.Utils.declare_queue(channel, "testQ", auto_delete: false, durable: false)
+
+    {:ok, %RabbitProducerState{amqp: amqp, channel: channel } }
+  end
+
+  def send(connection_id, message, from_device_seq) do
+    :gen_server.cast(:producer, {:send, connection_id, message, from_device_seq})
+  end
+
+  def handle_cast({:send, connection_id, message, from_device_seq}, state ) do
+    props = [headers: [{"connection_id", :longstr, connection_id}, {state.seq, :long, from_device_seq}]]
+    Exrabbit.Utils.publish(state.channel, "", "testQ", message, props)
+    {:noreply, state }
+  end
+
+  def terminate(reason, state) do
+    IO.puts "Rabbit producer terminating. Reason was:"
+    IO.inspect reason
+    Exrabbit.Utils.channel_close(state.channel)
+    Exrabbit.Utils.disconnect(state.amqp)
+    :ok
+  end
+
 
 #include_lib("./deps/gen_bunny/include/gen_bunny.hrl").
 
-use GenServer
+#use GenServer
 
-defmodule ExchangeDeclare do
-  require Record
-  defstruct(
-    Record.extract(:'exchange.declare', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  )
-end
+#defmodule ExchangeDeclare do
+#  require Record
+#  defstruct(
+#    Record.extract(:'exchange.declare', from_lib: "rabbit_common/include/rabbit_framing.hrl")
+#  )
+#end
 
 #-define(SERVER, ?MODULE). 
-defmodule State do
-  defstruct(
-    pid: nil
-  )
-end
+#defmodule State do
+#  defstruct(
+#    pid: nil
+#  )
+#end
 
-def start_link() do
-  :gen_server.start_link({:local, :rabbit}, __MODULE__, [], [])
-end
+#def start_link() do
+#  :gen_server.start_link({:local, :rabbit}, __MODULE__, [], [])
+#end
 
-def init([]) do
-  exchange = %ExchangeDeclare{exchange: <<"fanout">>, type: <<"fanout">>, durable: :true}
-  declareInfo = {exchange}
-  {:ok, pid} = :bunnyc.start_link(:mq_producer,
-    {:network, "localhost", 5672, {<<"guest">>, <<"guest">>}, <<"/">>},
-    declareInfo,
-    [] )
-  {:ok, %State{pid: pid}}
-end
+#def init([]) do
+#  exchange = %ExchangeDeclare{exchange: <<"fanout">>, type: <<"fanout">>, durable: :true}
+#  declareInfo = {exchange}
+#  {:ok, pid} = :bunnyc.start_link(:mq_producer,
+#    {:network, "localhost", 5672, {<<"guest">>, <<"guest">>}, <<"/">>},
+#    declareInfo,
+#    [] )
+#  {:ok, %State{pid: pid}}
+#end
 
-def handle_call({:send_message, msg}, _from, state) when is_list(msg) do
-  binMsg = :erlang.list_to_binary(msg)
-  :bunnyc.publish(:mq_producer, <<"myqueue">>, binMsg)
-  reply = :ok
-  {:reply, reply, state}
-end
+#def handle_call({:send_message, msg}, _from, state) when is_list(msg) do
+#  binMsg = :erlang.list_to_binary(msg)
+#  :bunnyc.publish(:mq_producer, <<"myqueue">>, binMsg)
+#  reply = :ok
+#  {:reply, reply, state}
+#end
 
-def handle_call({:send_message, msg}, _from, state) when is_binary(msg) do
-  :bunnyc.publish(:mq_producer, <<"myqueue">>, msg)
-  reply = :ok
-  {:reply, reply, state}
-end
+#def handle_call({:send_message, msg}, _from, state) when is_binary(msg) do
+#  :bunnyc.publish(:mq_producer, <<"myqueue">>, msg)
+#  reply = :ok
+#  {:reply, reply, state}
+#end
 
-def handle_call(request, _from, state) do
-  IO.format("Producer Request is: ~p~n", [request])
-  {:reply, :ok, state}
-end
+#def handle_call(request, _from, state) do
+#  IO.format("Producer Request is: ~p~n", [request])
+#  {:reply, :ok, state}
+#end
 
-def handle_cast(_msg, state) do
-  {:noreply, state}
-end
+#def handle_cast(_msg, state) do
+#  {:noreply, state}
+#end
 
-def handle_info(_info, state) do
-  {:noreply, state}
-end
+#def handle_info(_info, state) do
+#  {:noreply, state}
+#end
 
-def terminate(_reason, _state) do
-  :ok
-end
+#def terminate(_reason, _state) do
+#  :ok
+#end
 
-def code_change(_oldVsn, state, _extra) do
-  {:ok, state}
-end
+#def code_change(_oldVsn, state, _extra) do
+#  {:ok, state}
+#end
 
 #===================================================================
 
-def send_message(msg) do
-  :gen_server.call(__MODULE__ , {:send_message, msg })
-end
+#def send_message(msg) do
+#  :gen_server.call(__MODULE__ , {:send_message, msg })
+#end
 
 #===================================================================
 
