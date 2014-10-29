a5a3b5fd6fd8a77094a3bd9557c88eef0a4517c2
Sean <onewheel@gmail.com>
2014-10-29 20:54:18 +1300

    rabbit

--- elixir_umbrella/apps/webserver/lib/rabbit.producer.ex
+++ elixir_umbrella/apps/webserver/lib/rabbit.producer.ex
@@ -1,120 +1,49 @@
 defmodule Rabbit.Producer do
-  #use Behaviour
-  use GenServer
 
-  defmodule RabbitProducerState do
-    defstruct( amqp: nil, channel: nil, seq: 0 )
-  end
+defmodule Rabbit.AmqpParamsNetwork do
+  @record Record.Extractor.extract(:amqp_params_network, from: Path.join([Mix.Project.deps_path, "amqp_client", "include", "amqp_client.hrl"]))
+  defstruct @record
 
-  def start_link() do
-    #:gen_server.start_link({ :local, :Rabbit.Producer }, __MODULE__, [])
-    GenServer.start_link(__MODULE__, :ok, [])
-  end
-
-  def init(:ok) do
-    amqp = Exrabbit.Utils.connect()
-      #host: "localhost",
-      #port: 5672,
-      #username: "guest",
-      #password: "guest")
-
-    channel = Exrabbit.Utils.channel amqp
-    queue = Exrabbit.Utils.declare_queue(channel, "testQ", auto_delete: false, durable: false)
-
-    {:ok, %RabbitProducerState{amqp: amqp, channel: channel } }
-  end
-
-  def send(connection_id, message, from_device_seq) do
-    :gen_server.cast(:producer, {:send, connection_id, message, from_device_seq})
-  end
-
-  def handle_cast({:send, connection_id, message, from_device_seq}, state ) do
-    props = [headers: [{"connection_id", :longstr, connection_id}, {state.seq, :long, from_device_seq}]]
-    Exrabbit.Utils.publish(state.channel, "", "testQ", message, props)
-    {:noreply, state }
-  end
-
-  def terminate(reason, state) do
-    IO.puts "Rabbit producer terminating. Reason was:"
-    IO.inspect reason
-    Exrabbit.Utils.channel_close(state.channel)
-    Exrabbit.Utils.disconnect(state.amqp)
-    :ok
-  end
+  def record, do: @record
+end
 
 
-#include_lib("./deps/gen_bunny/include/gen_bunny.hrl").
 
-#use GenServer
+#-include("amqp_client.hrl").
 
-#defmodule ExchangeDeclare do
-#  require Record
-#  defstruct(
-#    Record.extract(:'exchange.declare', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-#  )
-#end
-
-#-define(SERVER, ?MODULE). 
-#defmodule State do
-#  defstruct(
-#    pid: nil
-#  )
-#end
-
-#def start_link() do
-#  :gen_server.start_link({:local, :rabbit}, __MODULE__, [], [])
-#end
-
-#def init([]) do
-#  exchange = %ExchangeDeclare{exchange: <<"fanout">>, type: <<"fanout">>, durable: :true}
-#  declareInfo = {exchange}
-#  {:ok, pid} = :bunnyc.start_link(:mq_producer,
-#    {:network, "localhost", 5672, {<<"guest">>, <<"guest">>}, <<"/">>},
-#    declareInfo,
-#    [] )
-#  {:ok, %State{pid: pid}}
-#end
-
-#def handle_call({:send_message, msg}, _from, state) when is_list(msg) do
-#  binMsg = :erlang.list_to_binary(msg)
-#  :bunnyc.publish(:mq_producer, <<"myqueue">>, binMsg)
-#  reply = :ok
-#  {:reply, reply, state}
-#end
-
-#def handle_call({:send_message, msg}, _from, state) when is_binary(msg) do
-#  :bunnyc.publish(:mq_producer, <<"myqueue">>, msg)
-#  reply = :ok
-#  {:reply, reply, state}
-#end
-
-#def handle_call(request, _from, state) do
-#  IO.format("Producer Request is: ~p~n", [request])
-#  {:reply, :ok, state}
-#end
-
-#def handle_cast(_msg, state) do
-#  {:noreply, state}
-#end
-
-#def handle_info(_info, state) do
-#  {:noreply, state}
-#end
-
-#def terminate(_reason, _state) do
-#  :ok
-#end
-
-#def code_change(_oldVsn, state, _extra) do
-#  {:ok, state}
-#end
-
-#===================================================================
-
-#def send_message(msg) do
-#  :gen_server.call(__MODULE__ , {:send_message, msg })
-#end
-
-#===================================================================
+#  def test() do
+#    # Start a network connection
+#    {:ok, connection} = :amqp_connection.start(:amqp_params_network{})
+#    # Open a channel on the connection
+#    {:ok, channel} = :amqp_connection.open_channel(connection)
+#
+#    # Declare a queue
+#    %'queue.declare_ok'{:queue = Q}
+#      = :amqp_channel.call(channel, %'queue.declare'{})
+#
+#    # Publish a message
+#    payload = <<"foobar">>
+#    publish = %'basic.publish'{:exchange = <<>>, :routing_key = q}
+#    :amqp_channel.cast(channel, publish, %amqp_msg{:payload = payload})
+#
+#    # Get the message back from the queue
+#    Get = %'basic.get'{:queue = Q}
+#    {#'basic.get_ok'{:delivery_tag = tag}, content}
+#       = :amqp_channel.call(channel, get)
+#
+#    # Do something with the message payload
+#    # (some work here)
+#
+#    # Ack the message
+#    :amqp_channel.cast(channel, %'basic.ack'{delivery_tag = tag})
+#
+#    # Close the channel
+#    :amqp_channel.close(channel)
+#    # Close the connection
+#    :amqp_connection.close(connection)
+#
+#    :ok
+#  end
 
 end
+
--- elixir_umbrella/apps/webserver/mix.exs
+++ elixir_umbrella/apps/webserver/mix.exs
@@ -13,7 +13,7 @@ defmodule Webserver.Mixfile do
   def application do
     # Starting :inets and :crypto first, or it will not start cowboy
     [applications:
-[:kernel, :stdlib, :lager, :ranch, :cowlib, :cowboy, :jiffy, :ssl, :ibrowse, :inets, :crypto, :riakc, :exprotobuf, :gpb ],
+[:kernel, :stdlib, :lager, :ranch, :cowlib, :cowboy, :jiffy, :ssl, :ibrowse, :inets, :crypto, :riakc ],
      mod: {Webserver, []},
      env:  [
         http_port: 8080,
@@ -35,15 +35,15 @@ defmodule Webserver.Mixfile do
       {:sync, github: "rustyio/sync" },
       #{:'riak-elixir-client', github: "drewkerrigan/riak-elixir-client" }
       {:riakc, github: "basho/riak-erlang-client" },
-      {:exprotobuf, github: "bitwalker/exprotobuf" },
-      {:gpb, github: "tomas-abrahamsson/gpb"},
+      {:exprotobuf, github: "bitwalker/exprotobuf", tag: "0.8.3"},
+      {:gpb, github: "tomas-abrahamsson/gpb", tag: "3.16.0", override: :true},
       #{:amqp_client, github: "mochi/amqp_client", override: :true},
-      #{:amqp_client, github: "jbrisbin/amqp_client" },
+      {:amqp_client, github: "jbrisbin/amqp_client" },
       #{:rabbit_common, github: "mochi/rabbit_common", override: :true, compile: :false, app: :false},
       #{:meck, github: "eproxus/meck"},
       #{:meck, github: "basho/meck", tag: "0.8.1p1", override: :true},
       #{:gen_bunny, github: "mochi/gen_bunny"},
-      {:exrabbit, github: "d0rc/exrabbit"},
+      #{:exrabbit, github: "d0rc/exrabbit"},
     ]
   end
 
