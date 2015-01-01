1ff022ce772d6b2d2b33b058aae6f6117036d988
Sean <onewheel@gmail.com>
2015-01-01 22:51:35 +1300

    perlin

--- elixir_umbrella/apps/world/lib/perlin_noise.ex
+++ elixir_umbrella/apps/world/lib/perlin_noise.ex
@@ -1,4 +1,5 @@
 ﻿defmodule PerlinNoise do
+use Bitwise
 
   # returns int
   # int minY, int maxY, float t
@@ -15,114 +16,101 @@
   # Returns int[][]
   # int minY, int maxY, float[][] perlinNoise
   defp MapInts(minY, maxY, perlinNoise) do
-    width = perlinNoise.Length;
-    height = perlinNoise[0].Length;
-    int[][] heightMap = GetEmptyArray<int>(width, height);
-
-    for (int i = 0; i < width; i++)
-    {
-      for (int j = 0; j < height; j++)
-      {
-        heightMap[i][j] = Interpolate(minY, maxY, perlinNoise[i][j]);
-      }
-    }
-    return heightMap;
+    #width = perlinNoise.Length;
+    #height = perlinNoise[0].Length;
+    #int[][] heightMap = GetEmptyArray<int>(width, height);
+    #for (int i = 0; i < width; i++)
+    #  for (int j = 0; j < height; j++)
+    #    heightMap[i][j] = Interpolate(minY, maxY, perlinNoise[i][j]);
+    #}
+    #return heightMap;
+    Enum.map(perlinNoise, fn {i} -> Interpolate(minY, maxY, i) end)
   end
 
-  # returns T[][]
+  # returns empty T[][]
   # int width, int height
   defp GetEmptyArray(width, height) do
-    var image = new T[width][];
+    Stream.repeatedly(fn -> 0 end) |> Enum.take(width * height)
+  end
 
-    for (int i = 0; i < width; i++)
-    {
-      image[i] = new T[height];
-    }
-    return image;
+  defp lookup(list, _width, height, x, y) do
+    :lists.nth(height * x + y, list)
   end
 
   # returns float[][]
   # float[][] baseNoise, int octave
-  defp GenerateSmoothNoise(baseNoise, octave) do
-    width = baseNoise.Length;
-    height = baseNoise[0].Length;
-
-    float[][] smoothNoise = GetEmptyArray<float>(width, height);
-    int samplePeriod = 1 << octave; // calculates 2 ^ k
-    float sampleFrequency = 1.0f / samplePeriod;
+  defp GenerateSmoothNoise(width, height, baseNoise, octave) do
+    smoothNoise = GetEmptyArray(width, height)
+    samplePeriod = 1 <<< octave  # calculates 2 ^ k
+    sampleFrequency = 1.0 / samplePeriod
 
     for (int i = 0; i < width; i++)
     {
       # calculate the horizontal sampling indices
-      int iSample0 = (i / samplePeriod) * samplePeriod;
-      int iSample1 = (iSample0 + samplePeriod) % width; //wrap around
-      float horizontalBlend = (i - iSample0) * sampleFrequency;
+      iSample0 = div(i, samplePeriod) * samplePeriod
+      iSample1 = rem( (iSample0 + samplePeriod), width) #wrap around
+      horizontalBlend = (i - iSample0) * sampleFrequency
 
       for (int j = 0; j < height; j++)
       {
         # calculate the vertical sampling indices
-        int jSample0 = (j / samplePeriod) * samplePeriod;
-        int jSample1 = (jSample0 + samplePeriod) % height; //wrap around
-        float verticalBlend = (j - jSample0) * sampleFrequency;
+        jSample0 = div(j, samplePeriod) * samplePeriod
+        jSample1 = rem( (jSample0 + samplePeriod), height) #wrap around
+        verticalBlend = (j - jSample0) * sampleFrequency
 
         # blend the top two corners
-        float top = Interpolate(baseNoise[iSample0][jSample0], baseNoise[iSample1][jSample0], horizontalBlend);
+        top = Interpolate(
+          lookup(baseNoise,width,height,iSample0,jSample0), 
+          lookup(baseNoise,width,height,iSample1,jSample0), 
+          horizontalBlend)
 
         # blend the bottom two corners
-        float bottom = Interpolate(baseNoise[iSample0][jSample1], baseNoise[iSample1][jSample1], horizontalBlend);
+        bottom = Interpolate(
+          lookup(baseNoise, width,height,iSample0,jSample1), 
+          lookup(baseNoise, width,height,iSample1,jSample1), 
+          horizontalBlend)
 
         # final blend
-        smoothNoise[i][j] = Interpolate(top, bottom, verticalBlend);
+        smoothNoise[i][j] = Interpolate(top, bottom, verticalBlend)
       }
     }
-    return smoothNoise;
+    return smoothNoise
   end
 
   # returns float[][] 
   # float[][] baseNoise, int octaveCount
-  defp GeneratePerlinNoise(baseNoise, octaveCount) do
-    width = baseNoise.Length;
-    height = baseNoise[0].Length;
-
-    var smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
-
-    const float PERSISTANCE = 0.4f;
+  defp GeneratePerlinNoise(width, height, baseNoise, octaveCount) do
+    smoothNoise = new float[octaveCount][][]; #an array of 2D arrays containing
+    PERSISTANCE = 0.4f
 
     # generate smooth noise
     for (int i = 0; i < octaveCount; i++)
     {
-      smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
+      smoothNoise[i] = GenerateSmoothNoise(width,height,baseNoise, i)
     }
 
-    float[][] perlinNoise = GetEmptyArray<float>(width, height); //an array of floats initialised to 0
+    perlinNoise = GetEmptyArray(width, height) #an array of floats initialised to 0
 
-    float amplitude = 1f;
-    float totalAmplitude = 0.0f;
+    amplitude = 1f
+    totalAmplitude = 0.0f
 
     # blend noise together
     for (int octave = octaveCount - 1; octave >= 0; octave--)
     {
-      amplitude *= PERSISTANCE;
-      totalAmplitude += amplitude;
+      amplitude = amplitude * PERSISTANCE
+      totalAmplitude = totalAmplitude + amplitude
 
       for (int i = 0; i < width; i++)
       {
         for (int j = 0; j < height; j++)
         {
-          perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+          perlinNoise[i][j] = perlinNoise[i][j] + smoothNoise[octave][i][j] * amplitude
         }
       }
     }
 
     # normalisation
-    for (int i = 0; i < width; i++)
-    {
-      for (int j = 0; j < height; j++)
-      {
-        perlinNoise[i][j] /= totalAmplitude;
-      }
-    }
-    return perlinNoise;
+    normalizedPerlinNoise = Enum.map(perlinNoise, fn {i} -> i / totalAmplitude end)
   end
 
   #--------------------------------------------------------------------------
@@ -134,9 +122,9 @@
   # returns int[][] 
   # int width, int height, int minY, int maxY, int octaveCount
   def GetIntMap(width, height, minY, maxY, octaveCount) do
-    baseNoise = GenerateWhiteNoise(width, height);
-    perlinNoise = GeneratePerlinNoise(baseNoise, octaveCount);
-    return MapInts(minY, maxY, perlinNoise);
+    baseNoise = GenerateWhiteNoise(width, height)
+    perlinNoise = GeneratePerlinNoise(width, height, baseNoise, octaveCount)
+    MapInts(minY, maxY, perlinNoise)
   end
 
   #--------------------------------------------------------------------------
